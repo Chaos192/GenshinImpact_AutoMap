d8bfcdabf36e789a15f805b1d27f749e35c3ae70
GengGode <GengGode@outlook.com>
2021-02-15 22:49:08 +0800

    添加半透明效果

--- GenshinImpact_AutoMap/GenshinImpact_AutoMap.cpp
+++ GenshinImpact_AutoMap/GenshinImpact_AutoMap.cpp
@@ -11,36 +11,17 @@ GenshinImpact_AutoMap::GenshinImpact_AutoMap(QWidget *parent)
 	connect(this, &GenshinImpact_AutoMap::mapUpdataFrontEnd, this, &GenshinImpact_AutoMap::updataFrontEnd);
 	connect(this, &GenshinImpact_AutoMap::mapUpdataBackEnd, this, &GenshinImpact_AutoMap::updataBackEnd);
 
-
-	//设置无边框
-	//setWindowFlags(Qt::CustomizeWindowHint);
-	setWindowFlags(Qt::FramelessWindowHint);
-	myCursor = new QCursor(QPixmap(":/CurMain/resource/cur3.cur"), 2,2);
+	//设置鼠标指针
+	myCursor = new QCursor(QPixmap(":/CurMain/resource/cur3.cur"), 2, 2);
 	this->setCursor(*myCursor);
-	//setWindowOpacity(0.7);
 
-	setWindowFlag(Qt::FramelessWindowHint); /* 注意：如果单纯开启窗口透明层效果，在Windows系统中必须设置, 其他系统可忽略。*/
-	setAttribute(Qt::WA_TranslucentBackground);
-
+	//设置半透明无边框窗口
+	//setWindowFlags(Qt::CustomizeWindowHint);
+	setWindowOpacity(0.9);
+	setWindowFlags(Qt::FramelessWindowHint);
 	this->setAttribute(Qt::WA_TranslucentBackground, true);
 
-	map.Mat2QImage();
-	//ui.MainView->setPixmap(QPixmap::fromImage(map.MainImg));
-
 	ui.MainView->setAttribute(Qt::WA_TranslucentBackground);
-
-	//label->setAttribute(Qt::WA_TranslucentBackground);
-
-	//label->setStyleSheet("background:transparent");
-
-	//map.getGiHandle();
-	//map.getThisHandle((HWND)this->winId());
-	//map.setWindowsPos();
-
-	//connect(this, &GenshinImpact_AutoMap::mapUpdata, this, &GenshinImpact_AutoMap::runMap);
-	//emit this->mapUpdata();
-	qDebug() << "Init finish";
-
 }
 
 //bool GenshinImpact_AutoMap::eventFilter(QObject * watched, QEvent * event)
@@ -108,18 +89,6 @@ void GenshinImpact_AutoMap::paintEvent(QPaintEvent * event)
 	//设置画面为地图
 	QPainter painter(this);
 	painter.drawImage(0, 0, map.MainImg);
-
-	//QPainter painter(this);
-	///* 0x20为透明层颜色，可自定义设置为0x0到0xff */
-	//painter.fillRect(this->rect(), QColor(0, 0, 0, 0x20));
-
-	//QPainter p(this);
-	//p.setCompositionMode(QPainter::CompositionMode_Clear);
-	//p.fillRect(10, 10, 300, 300, Qt::SolidPattern);
-}
-
-void GenshinImpact_AutoMap::EventClickMove()
-{
 }
 
 void GenshinImpact_AutoMap::runMap()
