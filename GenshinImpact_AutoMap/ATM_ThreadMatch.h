8035f0bf09e5f7f76feeb5314c7dbe83240414b1
GengGode <GengGode@outlook.com>
2021-02-20 00:12:25 +0800

    同步率下降？？？

--- GenshinImpact_AutoMap/ATM_ThreadMatch.h
+++ GenshinImpact_AutoMap/ATM_ThreadMatch.h
@@ -14,8 +14,8 @@ class ATM_TM_SurfMap
 	Mat _mapMat;
 	Mat _minMapMat;
 
-	int minHessian = 400;
-	float ratio_thresh = 0.75f;
+	int minHessian = 500;
+	float ratio_thresh = 0.66f;
 
 	Ptr<cv::xfeatures2d::SURF> detector, detectorSomeMap;
 	std::vector<KeyPoint> Kp_MinMap, Kp_Map, Kp_SomeMap;
