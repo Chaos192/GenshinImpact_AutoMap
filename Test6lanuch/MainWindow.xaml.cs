e0f18578b4f0b3ce7fdf412cb1fd7c09642fa7b6
GengGode <GengGode@outlook.com>
2021-02-25 20:53:16 +0800

    悬浮球启动器

---
+++ Test6lanuch/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test6lanuch
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            base.DragMove();//实现整个窗口的拖动
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
