75c96f0c56ddb9bc571dcf6aff41b4de39eba710
GengGode <GengGode@outlook.com>
2021-02-26 20:28:21 +0800

    添加选择原神窗口

---
+++ GenshinImpact_AutoMap/QtWidgetsClassMySelectGenshinImpactHandle.cpp
@@ -0,0 +1,11 @@
+#include "QtWidgetsClassMySelectGenshinImpactHandle.h"
+
+QtWidgetsClassMySelectGenshinImpactHandle::QtWidgetsClassMySelectGenshinImpactHandle(QWidget *parent)
+	: QWidget(parent)
+{
+	ui.setupUi(this);
+}
+
+QtWidgetsClassMySelectGenshinImpactHandle::~QtWidgetsClassMySelectGenshinImpactHandle()
+{
+}
