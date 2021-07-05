9ea3b5b2ad0d009f0e98d8a9f81dabca84e9cb16
GengGode <GengGode@outlook.com>
2021-07-06 04:00:45 +0800

    删除无用工程

--- 原神天理系统/QtGenshinImpactNaturalLaw.cpp
+++
@@ -1,8 +0,0 @@
-#include "QtGenshinImpactNaturalLaw.h"
-
-QtGenshinImpactNaturalLaw::QtGenshinImpactNaturalLaw(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-	setWindowFlags(Qt::FramelessWindowHint);
-}
